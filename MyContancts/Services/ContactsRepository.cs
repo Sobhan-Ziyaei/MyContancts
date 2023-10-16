87ab1545b330a5d4b90c1199dd3ebf2cc5b94363
Sobhan-Ziyaei <sobhan.ziaee73@gmail.com>
2023-10-16 10:22:58 +0330

    complete selectAll method to get data from DB

--- MyContancts/Services/ContactsRepository.cs
+++ MyContancts/Services/ContactsRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace MyContancts.Services
 {
     class ContactsRepository : IContactsRepository
     {
+        private string connectionString = "Data Source =.; Initial Catalog = Contact_DB; Integrated Security = true ";
+
         bool IContactsRepository.delete(int contactId)
         {
             throw new NotImplementedException();
@@ -22,7 +25,22 @@ namespace MyContancts.Services
 
         DataTable IContactsRepository.selectAll()
         {
-            throw new NotImplementedException();
+            string query = "Select * from MyContacts";
+            //درگاه اتصال به پایگاه داده را تعریف می‌کنیم
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            //اکنون می‌خواهیم بگوییم تمامی داده‌های پایگاه داده را برایم فراخوانی کن
+            //SqlDataAdapter -> یک وانت یا مخزن می‌باشد که داده‌های پایگاه داده را برایمان نگهداری می‌کند
+            //اکنون با استفاده از دستور زیر تمامی داده‌ها برایمان فراهم شده است
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+
+            //اکنون باید متغیری را تعریف کنیم تا داده‌ها را از مخزن دریافت کند و داخل خودش بریزد تا استفاده کنیم
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            //در نهایت می‌خواهیم این داده‌ها را برگردانیم
+            return data;
+
         }
 
         DataTable IContactsRepository.selectRow(int contactId)
