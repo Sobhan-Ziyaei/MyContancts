8570b70acf568c2519e4fc88cd09d8c0be692e25
Sobhan-Ziyaei <sobhan.ziaee73@gmail.com>
2023-10-16 09:50:51 +0330

    تست

--- MyContancts/Repository/IContactsRepository.cs
+++ MyContancts/Repository/IContactsRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,14 @@ namespace MyContancts.Repository
 {
     interface IContactsRepository
     {
+        bool insert(string name, string familyName, string mobile, string Email, int age, string address);
 
+        bool update(int contactId, string name, string familyName, string mobile, string Email, int age, string address);
+
+        bool delete(int contactId);
+
+        DataTable selectAll();
+
+        DataTable selectRow(int contactId);
     }
 }
---
+++ MyContancts/Services/ContactsRepository.cs
@@ -0,0 +1,38 @@
+﻿using MyContancts.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyContancts.Services
+{
+    class ContactsRepository : IContactsRepository
+    {
+        bool IContactsRepository.delete(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IContactsRepository.insert(string name, string familyName, string mobile, string Email, int age, string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        DataTable IContactsRepository.selectAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        DataTable IContactsRepository.selectRow(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IContactsRepository.update(int contactId, string name, string familyName, string mobile, string Email, int age, string address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
